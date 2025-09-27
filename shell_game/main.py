a1915fc5753e42d995dc25c84b4148fb41c1c812
Azize Dursun <143263029+yahusgame@users.noreply.github.com>
2025-09-28 00:48:59 +0300

    Update main.py

--- shell_game/main.py
+++ shell_game/main.py
@@ -6,30 +6,20 @@ LEVELS = [
     "questions/level2.json"
 ]
 
-def run_level(level_number, level_path):
-    print(f"\n== Level {level_number} Başlıyor! ==\n")
-    try:
-        questions = load_questions(level_path)
-    except FileNotFoundError:
-        print(f"Hata: {level_path} bulunamadı!")
-        return False
-    
-    game = Game(questions)
-    game.run()
-
-    if game.state.lives <= 0:
-        print("\n Oyunu Kaybettin. Tekrar dene!")
-        return False
-    else:
-        print(f"\n Level {level_number} Tamamlandı!")
-        return True
-
 def main():
     for i, level_path in enumerate(LEVELS, start=1):
-        if not run_level(i, level_path):
+        print(f"\n== Level {i} Başlıyor! ==\n")
+        questions = load_questions(level_path)
+        game = Game(questions)
+        game.run()
+
+        if game.state.lives <= 0:
+            print("\n Oyunu Kaybettin. Tekrar dene!")
             break
-        if i < len(LEVELS):
-            print("Sıradaki level yükleniyor...\n")
+        else:
+            print(f"\n Level {i} Tamamlandı!")
+            if i < len(LEVELS):
+                print("Sıradaki level yükleniyor...\n")
     else:
         print("\n 🎉 Tüm level'leri başarıyla bitirdin!")
 
