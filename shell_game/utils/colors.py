1abca5807ac8a74cf0091bbc00dff4ba820237f0
Azize Dursun <143263029+yahusgame@users.noreply.github.com>
2025-09-28 00:03:12 +0300

    Update colors.py

--- shell_game/utils/colors.py
+++ shell_game/utils/colors.py
@@ -1,14 +1,16 @@
-from colorama import Fore, init
+from colorama import Fore, Style, init
+
+# Renk ayarları
 init(autoreset=True, convert=True)
 
 def success(msg: str) -> str:
-    return Fore.GREEN + msg
+    return Fore.GREEN + Style.BRIGHT + "✅ " + msg
 
 def error(msg: str) -> str:
-    return Fore.RED + msg
+    return Fore.RED + Style.BRIGHT + "❌ " + msg
 
 def info(msg: str) -> str:
-    return Fore.CYAN + msg
+    return Fore.CYAN + "ℹ️ " + msg
 
 def warning(msg: str) -> str:
-    return Fore.YELLOW + msg
+    return Fore.YELLOW + "💡 " + msg
